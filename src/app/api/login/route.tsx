ab68c031ea3b67e7386cf4d53554e0a63765bf97
Gustavo Coimbra <whoisgustavolima@gmail.com>
2024-07-11 14:54:57 -0300

    my-finances

--- src/app/api/login/route.tsx
+++ src/app/api/login/route.tsx
@@ -8,8 +8,9 @@ export async function GET(request: Request) {
     const urlParams = new URLSearchParams(request.url.split('?')[1]);
     const email = urlParams.get('email');    
     const password = urlParams.get('password');    
-    
+     
     const data = await sql`SELECT md5(users.email||users.password) AS token FROM users WHERE email = ${email} AND password = md5(${password})`;
+    
     let response;
     let token;
     if (token = data.rows[0].token) {
--- src/app/api/transactions/route.tsx
+++ src/app/api/transactions/route.tsx
@@ -8,85 +8,106 @@ export async function GET(request: Request) {
     const urlParams = new URLSearchParams(request.url.split('?')[1]);
     const token = urlParams.get('token');
     const date = urlParams.get('date'); 
-    let startDate;
-    let endDate;
+    var startDate:string;
+    var endDate:string;
     if (date) {
+
         const [yearStr, monthStr] = date.split('-');
         const year = parseInt(yearStr, 10); 
         const month = parseInt(monthStr, 10); 
         const lastDay = new Date(year, month, 0).getDate(); 
         startDate = `${yearStr}-${monthStr}-01`;
         endDate = `${yearStr}-${monthStr}-${lastDay}`;
-    }
 
-    const checkRecurring = await sql`
-        SELECT 
-            transactions.*
-        FROM transactions
-        INNER JOIN users ON users.id = transactions.user_id
-        WHERE md5(users.email||users.password) = ${token} 
-        AND transactions.recurring = TRUE AND from_transaction = 0 AND transactions.date <= ${endDate}
-    `;
-
-    if(checkRecurring.rows.length) {
-
-
-        const currentPeriod = await sql`
+        // Percorre transações com recorrência ativada
+        const checkRecurring = await sql`
             SELECT 
-                count(*) found
+                transactions.*
             FROM transactions
             INNER JOIN users ON users.id = transactions.user_id
             WHERE md5(users.email||users.password) = ${token} 
-            AND transactions.recurring = true
-            AND (transactions.from_transaction = ${checkRecurring.rows[0].id} OR transactions.id = ${checkRecurring.rows[0].id})
-            AND transactions.date >= ${startDate} AND transactions.date <= ${endDate} 
+            AND transactions.recurring = TRUE AND from_transaction = 0 AND transactions.date <= ${endDate}
         `;
 
-        if (currentPeriod.rows[0].found == "0") {
-            const urlParams = new URLSearchParams(request.url.split('?')[1]);
-            const date = urlParams.get('date'); 
-            if (date) {
-                const [yearStr, monthStr] = date.split('-');
-                const year = parseInt(yearStr, 10); 
-                const month = parseInt(monthStr, 10); 
-                const newDate = `${year}-${month}-${checkRecurring.rows[0].date.toISOString().slice(8,10)}`;
-                await sql`INSERT INTO transactions (user_id, description, value, date, type, recurring, from_transaction) VALUES (${checkRecurring.rows[0].user_id}, ${checkRecurring.rows[0].description},${checkRecurring.rows[0].value},${newDate},${checkRecurring.rows[0].type},${checkRecurring.rows[0].recurring},${checkRecurring.rows[0].id})`;
+        checkRecurring.rows.forEach(async (row) => {
+            
+            // Verificar se a transação foi clonada
+            const currentPeriod = await sql`
+                SELECT 
+                    transactions.*
+                FROM transactions
+                INNER JOIN users ON users.id = transactions.user_id
+                WHERE md5(users.email || users.password) = ${token}
+                AND transactions.recurring = true
+                AND transactions.from_transaction = ${row.id}
+                AND transactions.date >= ${startDate}
+                AND transactions.date <= ${endDate}
+                AND NOT EXISTS (
+                    SELECT 1 
+                    FROM transactions t2
+                    WHERE t2.from_transaction = transactions.id
+                );
+            `;
+
+            if (currentPeriod.rows.length == 0) {
+
+                // Insere 
+                const urlParams = new URLSearchParams(request.url.split('?')[1]);
+                const date = urlParams.get('date'); 
+                if (date) {
+                    const [yearStr, monthStr] = date.split('-');
+                    const year = parseInt(yearStr, 10); 
+                    const month = parseInt(monthStr, 10); 
+                    const newDate = `${year}-${month}-${row.date.toISOString().slice(8,10)}`;
+                    sql`INSERT INTO transactions (user_id, description, value, date, type, recurring, from_transaction) VALUES (${row.user_id}, ${row.description},${row.value},${newDate},${row.type},${row.recurring},${row.id})`;
+                }
+            }
+     
+        })
+
+        const data = await sql`
+            SELECT 
+                transactions.id,
+                transactions.description,
+                transactions.value,
+                transactions.date,
+                transactions.type,
+                transactions.recurring,
+                transactions.paid
+            FROM transactions
+            INNER JOIN users ON users.id = transactions.user_id
+            WHERE md5(users.email||users.password) = ${token} 
+            AND transactions.date >= ${startDate} AND transactions.date <= ${endDate}
+        `;
+
+        let response;
+
+        if (data.rows.length) {
+            response = {
+                code: 1,
+                count: data.rows.length,
+                message: `${data.rows.length} transactions found`,
+                transactions: data.rows,
+            }
+        } else {
+            response = {
+                code: 2,
+                count: 0,
+                message: `No transactions found`,
             }
         }
+
+        return NextResponse.json(response, { status: 200 });
+
     }
 
-    const data = await sql`
-        SELECT 
-            transactions.id,
-            transactions.description,
-            transactions.value,
-            transactions.date,
-            transactions.type,
-            transactions.recurring,
-            transactions.paid
-        FROM transactions
-        INNER JOIN users ON users.id = transactions.user_id
-        WHERE md5(users.email||users.password) = ${token} 
-        AND transactions.date >= ${startDate} AND transactions.date <= ${endDate}
-    `;
-
-    let response;
-
-    if (data.rows.length) {
-        response = {
-            code: 1,
-            count: data.rows.length,
-            message: `${data.rows.length} transactions found`,
-            transactions: data.rows,
-        }
-    } else {
-        response = {
-            code: 2,
-            count: 0,
-            message: `No transactions found`,
-        }
+    const response = {
+        code: 2,
+        count: 0,
+        message: `No transactions found`,
     }
 
     return NextResponse.json(response, { status: 200 });
+
 }
 
